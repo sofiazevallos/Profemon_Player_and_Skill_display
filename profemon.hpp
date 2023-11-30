7927b4f8f5f28108bc04b1979ba3d47517aa7719
sofiazevallos <116527734+sofiazevallos@users.noreply.github.com>
2023-11-30 12:06:33 -0500

    Update profemon.hpp

--- profemon.hpp
+++ profemon.hpp
@@ -1,35 +1,53 @@
 //Author: Sofia Zevallos
 //Interface of the Profemon class
+
 #include <iostream>
 #include <string>
-#include "skill.hpp" 
+// Include the header file that contains the Skill class declaration
+#include "skill.hpp"
+
+// Include guards
 #ifndef PROFEMON_HPP
 #define PROFEMON_HPP
 
-enum Specialty{ML, SOFTWARE, HARDWARE};
+// Enumerated type (enum) representing possible specialties for Profemon
+enum Specialty {ML, SOFTWARE, HARDWARE};
 
+// Class definition for the Profemon class
 class Profemon
 {
     public:
+        // Default constructor
         Profemon();
+
+        // Parameterized constructor
         Profemon(std::string name, double max_health, Specialty specialty);
+
+        // Getter functions
         std::string getName();
         Specialty getSpecialty();
         int getLevel();
         double getMaxHealth();
+
+        // Setter functions
         void setName(std::string name);
         void levelUp(int exp);
         bool learnSkill(int slot, Skill skill);
         void printProfemon(bool print_skills);
 
     private:
+        // Private member variables
         std::string profemon_name;
         int profemon_level;
         int level_up_exp;
         int current_exp;
         double max_health_;
         Specialty profemon_specialty;
-        Skill learned_skills[3]; 
+        Skill learned_skills[3]; // Array to store up to three learned skills
 };
-#endif 
+
+// End of the class definition
+
+// End of include guards
+#endif
 
