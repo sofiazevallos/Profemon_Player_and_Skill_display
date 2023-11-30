61f15553abe6737a2f5095cde823c0c8a3e71ac2
sofiazevallos <116527734+sofiazevallos@users.noreply.github.com>
2023-11-30 11:59:45 -0500

    Update skill.hpp

--- skill.hpp
+++ skill.hpp
@@ -5,24 +5,35 @@
 #ifndef SKILL_HPP
 #define SKILL_HPP
 
+//class definition
 class Skill
 {
     public:
+        // Default constructor
         Skill(); 
+
+        // Parameterized constructor
         Skill(std::string name, std::string description, int specialty, int uses);
+
+        // Getter functions
         std::string getName();
         std::string getDescription();
         int getTotalUses();
         int getSpecialty();
+
+        // Setter functions
         void setName(std::string name);
         void setDescription(std::string description);
         void setTotalUses(int uses);
         bool setSpecialty(int specialty);
     
     private:
+        // Private member variables
         std::string skill_name;
         std::string skill_description;
         int skill_use;
         int skill_specialty; 
 };
+// End of the class definition
+// End of include guards
 #endif 
